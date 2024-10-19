be4dcef78d8673d01d97a931837726485f390836
Shibata Hironori <46587015+hilosiva@users.noreply.github.com>
2024-10-19 11:12:29 +0900

    update: Astro 4.16.6 + Lightning CSS、Biome

--- src/assets/js/common.js
+++
@@ -1,4 +0,0 @@
-import Toggle from "./libs/Toggle";
-import Loading from "./libs/Loading";
-
-export { Toggle, Loading };
--- src/config.ts
+++ src/config.ts
@@ -5,23 +5,41 @@ export const SITE = {
   name: "Orelop Astro",
   description: "Webサイトの説明文",
   url: "https://example.com",
-  base: "/",
-  app: {
-    turbo: false,
-    momentumScroll: false,
-  },
+  base: "",
+  trackingId: "G-XXXXXX",
+  viewTransition: true,
 };
 
 // ================================
 // 会社情報
 // ================================
 export const COMPANY = {
-  name: "Orelop",
+  name: "会社名",
+  founder: "代表者",
+  description: "会社紹介",
+  foundingDate: "設立年月",
+  telephone: "+81-XX-XXX-XXXX",
+  fax: "+81-XX-XXX-XXXX",
+  email: "info@example.com",
+  address: {
+    addressLocality: "市町村",
+    addressRegion: "都道府県",
+    postalCode: "郵便番号",
+    streetAddress: "番地",
+    addressCountry: "JP",
+  },
+  contactPoint: [
+    {
+      telephone: "+81-XX-XXX-XXXX",
+    },
+  ],
 };
 
 // ================================
 // SNS情報
 // ================================
 export const SNS = {
-  twitter: "@",
+  twitter: null,
+  instagram: null,
+  facebook: null,
 };
